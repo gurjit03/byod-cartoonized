4b50ae74ea5105ff04367f4c38e162cb6dd03fd2
gurjit singh <gurjitmehta@gmail.com>
2021-08-26 23:02:19 +0530

    fix: server crashing issue, removes userroutes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // HOME PAGE
 router.get('/', function(req, res) {
   const filename = req.query.filename;
-  res.send(filename)
+  res.send(filename ? filename : "Please add the filename in the query param to continue")
 });
 
 module.exports = router;
