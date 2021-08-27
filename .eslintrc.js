a9edbeb35c00fb8c674970cfded68351cf1c7872
gurjit singh <gurjitmehta@gmail.com>
2021-08-28 00:29:17 +0530

    fix: eslint & prettierconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
   env: {
-    es6: true,
-    node: true
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
+  extends: [
+    'prettier',
+    'airbnb-base',
+  ],
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
+    ecmaVersion: 12,
   },
+  plugins: [
+  "prettier"
+  ],
   rules: {
-    'prettier/prettier': 'error',
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
-  }
+    "prettier/prettier": "error"
+  },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,43 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
+const fs = require('fs').promises;
+const execShellCommand = require('../utils/exec-shell-command');
 
 // HOME PAGE
-router.get('/', function(req, res) {
+router.get('/', async function (req, res) {
   const filename = req.query.filename;
-  res.send(filename ? filename : "Please add the filename in the query param to continue")
+  res.setHeader('Content-Type', 'text/plain');
+  if (filename) {
+    const filepath = path.resolve('./', 'public', 'images', filename);
+    try {
+      const filestats = await fs.stat(filepath);
+      if (filestats) {
+        const splittedFilename = filename.split('.');
+        const filenameWithoutExtension = splittedFilename[0];
+        const fileExtension = splittedFilename[1];
+        const convertedFilepath = path.resolve(
+          './',
+          'public',
+          'images',
+          `${filenameWithoutExtension}.${fileExtension}`
+        );
+        const convertedFilepath = file + '-converted';
+        // create cartoonized version of the image added in the query param
+        await execShellCommand(
+          `convert ${filepath} -kuwahara RadiusxSigma resultimage`
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+      res.send('file is not available on the server');
+    }
+  }
+  res.send(
+    filename
+      ? filename
+      : 'Please add the filename in the query param to continue'
+  );
 });
 
 module.exports = router;
