a22abae31d9f654ab3156471b8be311aa292fbbe
gurjit singh <gurjitmehta@gmail.com>
2021-09-02 22:45:36 +0530

    chore: removes require exec outside function

--- utils/exec-shell-command.js
+++ utils/exec-shell-command.js
@@ -1,13 +1,14 @@
+const exec = require('child_process').exec;
+
 function execShellCommand(cmd) {
- const exec = require('child_process').exec;
- return new Promise((resolve, reject) => {
-  exec(cmd, (error, stdout, stderr) => {
-   if (error) {
-    console.warn(error);
-   }
-   resolve(stdout? stdout : stderr);
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error);
+      }
+      resolve(stdout ? stdout : stderr);
+    });
   });
- });
 }
 
-module.exports = execShellCommand
+module.exports = execShellCommand;
