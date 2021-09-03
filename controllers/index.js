308865b36e7014649b190f49873a8412e1bed8c0
gurjit singh <gurjitmehta@gmail.com>
2021-09-03 21:13:45 +0530

    chore: creates controller

---
+++ controllers/index.js
@@ -0,0 +1,35 @@
+const applyFilters = require('../utils/apply-filter');
+
+async function uploadFileController(req, res) {
+  try {
+    const uploadedFile = req.file;
+
+    // make sure file is available
+    if (!uploadedFile) {
+      res.status(400).send({
+        status: false,
+        data: 'No file is selected.',
+      });
+    } else {
+      try {
+        await applyFilters(uploadedFile.originalname);
+      } catch (error) {
+        res.status(500).send('File not available on the server');
+      }
+
+      // send response
+      res.status(200).send({
+        message: 'File is uploaded.',
+        data: {
+          name: uploadedFile.originalname,
+          mimetype: uploadedFile.mimetype,
+          size: uploadedFile.size,
+        },
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+module.exports = uploadFileController;
--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,8 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
-const fs = require('fs').promises;
+const uploadFileController = require('../controllers');
 const upload = require('../lib/multer');
-const applyFilters = require('../utils/apply-filter');
 
-router.post(
-  '/upload',
-  upload.single('uploadedFile'),
-  async function (req, res) {
-    try {
-      const uploadedFile = req.file;
-
-      // make sure file is available
-      if (!uploadedFile) {
-        res.status(400).send({
-          status: false,
-          data: 'No file is selected.',
-        });
-      } else {
-        try {
-          await applyFilters(uploadedFile.originalname);
-        } catch (error) {
-          res.status(500).send('File not available on the server');
-        }
-
-        // send response
-        res.status(200).send({
-          message: 'File is uploaded.',
-          data: {
-            name: uploadedFile.originalname,
-            mimetype: uploadedFile.mimetype,
-            size: uploadedFile.size,
-          },
-        });
-      }
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  }
-);
+router.post('/upload', upload.single('uploadedFile'), uploadFileController);
 
 module.exports = router;
